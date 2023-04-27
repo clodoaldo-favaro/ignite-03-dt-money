f313dc41c0f300cf6871b69c3eecc2c054fbd838
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2023-04-27 00:50:59 -0300

    chore: set global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
-  return (
-    <h1>Hello Darkness</h1>
-  )
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<h1>Hello Darkness</h1>
+		</ThemeProvider>
+	)
 }
