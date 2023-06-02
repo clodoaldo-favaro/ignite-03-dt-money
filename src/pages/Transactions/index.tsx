afb52e2f3c4105f488df0867766b488ed5135ad1
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2023-06-01 22:56:27 -0300

    feat: barra de pesquisa

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { TransactionsContainer, TransactionTable, PriceHighlight } from "./styles";
 
 export function Transactions() {
@@ -9,6 +10,7 @@ export function Transactions() {
 			<Summary />
 
 			<TransactionsContainer>
+				<SearchForm />
 				<TransactionTable>
 					<tbody>
 						<tr>
